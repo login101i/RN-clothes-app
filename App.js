d1dea928aa46c26897589747d94c747d0b08090c
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2020-10-24 23:50:03 +0200

    navigation update

--- App.js
+++ App.js
@@ -1,41 +1,14 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons'
-import Swipeable from 'react-native-gesture-handler/Swipeable';
-import * as ImagePicker from 'expo-image-picker'
-import * as Permissions from 'expo-permissions'
-
-
-import Screen from './app/components/Screen'
-import ImageInput from './app/components/ImageInput'
-import ImageInputList from './app/components/ImageInputList'
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
-
-  const [imageUris, setImageUris] = useState([])
-
-  console.log(imageUris)
-
-  handleAdd = uri => {
-    setImageUris([...imageUris, uri])
-  }
-
-  handleRemove = uri => {
-    setImageUris(imageUris.filter(imageUri => imageUri !== uri))
-  }
-
-
-
-
-
   return (
-
-    <Screen>
-      <ListingEditScreen />
-    </Screen>
-
-  )
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
-
